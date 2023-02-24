6870c1f5e9ee09a6dbfd2b3e695534e25742f757
ferhatergun <eferhat718@gmail.com>
2023-02-24 18:42:56 +0300

    Animal-Sounds

---
+++ index.js
@@ -0,0 +1,75 @@
+function bird()
+{
+    var audio = new Audio('https://www.google.com/logos/fnbx/animal_sounds/robin.mp3');
+    audio.play();
+}
+function frog()
+{
+    var audio = new Audio('https://www.google.com/logos/fnbx/animal_sounds/frog.mp3');
+    audio.play();
+}
+function horoz()
+{
+    var audio = new Audio('https://www.google.com/logos/fnbx/animal_sounds/rooster.mp3');
+    audio.play();
+}
+function elephant()
+{
+    var audio = new Audio('https://www.google.com/logos/fnbx/animal_sounds/elephant.mp3');
+    audio.play();
+}
+function horse()
+{
+    var audio = new Audio('./sounds/horse.wav');
+    audio.play();
+}
+function dog()
+{
+    var audio = new Audio('./sounds/dog.wav');
+    audio.play();
+}
+function cow()
+{
+    var audio = new Audio('./sounds/cow.wav');
+    audio.play();
+}
+function koyun()
+{
+    var audio = new Audio('./sounds/koyun.mp3');
+    audio.play();
+}
+function wolf()
+{
+    var audio = new Audio('./sounds/wolf.mp3');
+    audio.play();
+}
+function donkey()
+{
+    var audio = new Audio('./sounds/donkey.mp3');
+    audio.play();
+}
+function eagle()
+{
+    var audio = new Audio('./sounds/eagle.mp3');
+    audio.play();
+}
+function marti()
+{
+    var audio = new Audio('./sounds/marti.mp3');
+    audio.play();
+}
+function cat()
+{
+    var audio = new Audio('./sounds/cat.mp3');
+    audio.play();
+}
+function monkey()
+{
+    var audio = new Audio('./sounds/monkey.mp3');
+    audio.play();
+}
+function lion()
+{
+    var audio = new Audio('./sounds/lion.mp3');
+    audio.play();
+}
